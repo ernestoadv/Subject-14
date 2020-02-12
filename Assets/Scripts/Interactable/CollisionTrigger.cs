aefa797e06a1b6399148c8e84be147801b87d337
Ernesto Aranda <ernestoadv@icloud.com>
2020-02-12 23:36:40 +0200

    Transitions done
    - Transitions between levels
    - Animated transitions

---
+++ Assets/Scripts/Interactable/CollisionTrigger.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour
+{
+    public Animator animator;
+
+    //This function runs when the player ends the level (by colliding with the door)
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        animator.updateMode = AnimatorUpdateMode.UnscaledTime; //So that the animation does not freeze when pausing the game
+        Time.timeScale = 0; //Freeze the game
+        animator.SetTrigger("fade"); //Fade in
+    }
+}
---
+++ Assets/Scripts/Interactable/LevelChange.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour
+{
+    public Animator animator;
+    public int levelToLoad;
+
+    //Loads new level
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad); //Load new level
+        Time.timeScale = 1; //Unfreeze the game
+    }
+}
