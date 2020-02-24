f863a0298faa0672da80b64c93368b961b63e648
migue0418 <miguealguacil@gmail.com>
2020-02-24 16:25:06 +0200

    Camera Update Level 1
    - Create new script for camera, only follow player (is able to go back)

---
+++ Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Camera camera;
+    public Transform Player;
+    public Transform initialPositionOfCamera, endPositionOfCamera;
+    public float height = 0f;
+    private bool attachCamera;
+
+    private void Start()
+    {
+        transform.position = new Vector3(initialPositionOfCamera.position.x, height, -12);
+    }
+/*
+    bool UpdatedAttached()
+    {
+        // the camera won't move until the player reaches the camera initial position
+        if (Player.position.x <= initialPositionOfCamera.position.x)
+        {
+            attachCamera = false;
+        }
+        else
+        {
+            // area between the initial position of the camera and the end of the wave
+            attachCamera = true;
+        }
+
+        return attachCamera;
+    }
+*/
+    private void Update()
+    {
+        if (Player.position.x > initialPositionOfCamera.position.x && Player.position.x <= endPositionOfCamera.position.x)
+        {
+            transform.position = new Vector3(Player.position.x, height, -12);
+        }
+        
+    }
+}
